f2200924c03957d9d52bec99bd99e3c92e79129e
HolyDarknessMystic721 <70783250+HolyDarknessMystic721@users.noreply.github.com>
2020-09-19 19:04:14 -0400

    Added Destroying a basket

--- Assets/Scripts/ApplePicker.cs
+++ Assets/Scripts/ApplePicker.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class ApplePicker : MonoBehaviour
 {
@@ -10,14 +12,20 @@ public class ApplePicker : MonoBehaviour
 	public int 		numBaskets = 3;
 	public float	basketBottomY = -14f;
 	public float	basketSpacingY = 2f;
+	public List<GameObject> basketList;
+
     // Start is called before the first frame update
     void Start()
     {
+    	basketList = new List<GameObject>();
+
         for (int i=0; i<numBaskets; i++){
 			GameObject tBasketGO = Instantiate<GameObject>(basketPrefab);
 			Vector3 pos = Vector3.zero;
 			pos.y = basketBottomY + ( basketSpacingY * i);
 			tBasketGO.transform.position = pos;
+
+			basketList.Add(tBasketGO);
 		}
     }
     public void AppleDestroyed() {
@@ -26,5 +34,14 @@ public class ApplePicker : MonoBehaviour
 		foreach (GameObject tGO in tAppleArray) {
                Destroy(tGO);
            }
+
+          	// Destroy one of the baskets
+			// Get the index of the last Basket in backetList
+			int basketIndex = basketList.Count-1; 
+			// Get a reference to that Basket GameObject
+			GameObject tBasketGO = basketList[basketIndex];
+			// Remove the Basket from the list and destroy the GameObject
+			basketList.RemoveAt(basketIndex); 
+			Destroy(tBasketGO);
        }
 }
